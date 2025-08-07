8d6de2241e0b0544ca94d8b1a9f3ba5de16793a7
Pepe Campos <jose.campos.cuina@alumnos.upm.es>
2025-08-07 19:27:44 +0200

    change

---
+++ docs/source/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../../src"))  # si tu código está en src/
+
+project = "2025_AI_Gait_Identification"
+copyright = "2025, Jose Campos Cuiña"
+author = "Jose Campos Cuiña"
+release = "1.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+]
+
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+language = "english"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
