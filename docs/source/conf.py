312a18b758fcb6ae9fc4e463293046ec0cdcc463
Pepe Campos <jose.campos.cuina@alumnos.upm.es>
2025-08-18 11:55:44 +0200

    change

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,7 +8,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(0, os.path.abspath("../src"))
 
 project = "2025_AI_Gait_Identification"
 copyright = "2025, Jose Campos Cuiña"
