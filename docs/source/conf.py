1a56246ec5cb5100f87ffe3224df11c615d08ea6
Pepe Campos <jose.campos.cuina@alumnos.upm.es>
2025-08-18 12:18:37 +0200

    change

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,33 +1,33 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# For the full list of built-in configuration values, see the documentation:
+# For the full list of built-in configuration values, see:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-import os
 import sys
-
-sys.path.insert(0, os.path.abspath("../src"))
-
+from pathlib import Path
 import importlib.util
 
+# -- Path setup --------------------------------------------------------------
+# Add the project root so "import src" works
+ROOT = Path(__file__).resolve().parents[2]
+sys.path.insert(0, str(ROOT))
+
+# Debug: check if modules can be found
 for module in [
-    "inference",
-    "encode_latent_transformer",
-    "train_autoencoder",
+    "src.inference",
+    "src.encode_latent_transformer",
+    "src.train_autoencoder",
 ]:
     spec = importlib.util.find_spec(module)
     print(f"[SPHINX DEBUG] {module} found:", spec is not None)
 
+# -- Project information -----------------------------------------------------
 project = "2025_AI_Gait_Identification"
 copyright = "2025, Jose Campos Cuiña"
 author = "Jose Campos Cuiña"
 release = "1.0"
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
@@ -38,8 +38,9 @@ exclude_patterns = []
 
 language = "english"
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+# If some heavy dependencies break imports, mock them here:
+# autodoc_mock_imports = ["tensorflow", "torch", "sklearn"]
 
+# -- Options for HTML output -------------------------------------------------
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
